754e0a9a8dc40cc83d65cfe669d20ff2277bc4b6
vinitlad11 <92136889+vinitlad11@users.noreply.github.com>
2024-02-12 15:45:03 +0530

    added json file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,17 @@
 import "./App.css";
 import Card from "./components/Card";
 import Home from "./components/Home";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import Index from "./components/Index";
 
 function App() {
   return (
-    <BrowserRouter basename="/:name">
-      <Routes>
-        <Route path="/link" element={<Index />} />;
-        <Route path="/card/:name" element={<Card />} />;
-        <Route path="/" element={<Home />} />;
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />;
+      <Route path="/card/:name" element={<Card />} />;
+      <Route path="/:name" element={<Home />} />;
+    </Routes>
   );
 }
 
