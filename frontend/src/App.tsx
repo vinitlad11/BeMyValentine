18dd1cc98825fe27ac65c5b77dfcc8f3c8a12096
vinitlad11 <92136889+vinitlad11@users.noreply.github.com>
2024-02-12 15:39:57 +0530

    browser route try

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import Card from "./components/Card";
 import Home from "./components/Home";
-import { Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
-// import Index from "./components/Index";
+import Index from "./components/Index";
 
 function App() {
   return (
-    <Routes>
-      {/* <Route path="/" element={<Index />} />; */}
-      <Route path="/card/:name" element={<Card />} />;
-      <Route path="/:name" element={<Home />} />;
-    </Routes>
+    <BrowserRouter basename="/:name">
+      <Routes>
+        <Route path="/link" element={<Index />} />;
+        <Route path="/card/:name" element={<Card />} />;
+        <Route path="/" element={<Home />} />;
+      </Routes>
+    </BrowserRouter>
   );
 }
 
