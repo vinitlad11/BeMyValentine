42889da52c2802fb2ed251aeaf5725311e13e071
vinitlad11 <92136889+vinitlad11@users.noreply.github.com>
2024-02-12 15:31:32 +0530

    route change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,12 @@ import Card from "./components/Card";
 import Home from "./components/Home";
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
-import Index from "./components/Index";
+// import Index from "./components/Index";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Index />} />;
+      {/* <Route path="/" element={<Index />} />; */}
       <Route path="/card/:name" element={<Card />} />;
       <Route path="/:name" element={<Home />} />;
     </Routes>
